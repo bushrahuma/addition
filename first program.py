cd4c20dce51eb0e2eee75294dcec95854d294faa
bushra <bushrahuma007@gmail.com>
2021-06-16 06:13:20 +0500

    initial commit

---
+++ first program.py
@@ -0,0 +1,11 @@
+name=input('please enter employee name')
+print(name)
+employee_id=input('enter employee id')
+print(int(employee_id))
+commission_rate=input('enter commission_rate')
+print(float(commission_rate))
+shift=input('please enter shift')
+if shift=='morning':
+    print('morning shift')
+else:
+    print('enening shift')
